322107157aa0523979ea19cd10d66dd316ddb29f
root <root@ns390143.ip-188-165-240.eu>
2014-10-01 01:17:35 +0200

    Front-end update: responsive web design basic template

--- zend/public/js/frontend.js
+++ zend/public/js/frontend.js
@@ -2,3 +2,8 @@
  * @author Michal Gacki 
  * @description JS for AngularJS
  */
+var WalletApp = angular.module('Wallet', ['ngResource']);
+
+WalletApp.controller('AppController', function ($scope) {
+    
+});
