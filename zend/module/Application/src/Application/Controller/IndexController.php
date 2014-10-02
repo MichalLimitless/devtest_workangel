54c6923d01fea5ca169a2503815511d143e54e5c
root <root@ns390143.ip-188-165-240.eu>
2014-10-02 05:20:38 +0200

    Modified: Homepage welcome message for guest and log in user

--- zend/module/Application/src/Application/Controller/IndexController.php
+++ zend/module/Application/src/Application/Controller/IndexController.php
@@ -16,11 +16,56 @@ use Zend\Form\Fieldset;
 use Zend\Form\Form;
 use Zend\InputFilter\Input;
 use Zend\InputFilter\InputFilter;
+use User\Model\User;
+use \User\Controller\UserController;
 
 /**
  * Main application
  */
 class IndexController extends AbstractActionController {
+    
+    /**
+     * User table object
+     * @var UserTable
+     */
+    protected $userTable;
+    
+    /**
+     * User instance
+     * @var User
+     */
+    protected $userInstance = null;
+    
+    /**
+     * Get User table from Zend Service Manager
+     * @return UserTable
+     */
+    public function getUserTable() {
+
+        if (!$this->userTable) {
+            $sm = $this->getServiceLocator();
+            $this->userTable = $sm->get('User\Model\UserTable');
+        }
+
+        return $this->userTable;
+        
+    }
+    
+    /**
+     * Get user controller
+     * @return UserController
+     */
+    public function getUserModule() {
+        
+        if ($this->userInstance === null) {
+            $userTable = $this->getUserTable();
+            $this->userInstance = new \User\Controller\UserController();
+            $this->userInstance->setUserTable($userTable);
+        }
+        
+        return $this->userInstance;
+        
+    }
 
     /**
      * Homepage action - welcome page
@@ -29,8 +74,25 @@ class IndexController extends AbstractActionController {
      * @return ViewModel
      */
     public function indexAction() {
+        error_reporting(E_ALL);
+ini_set('display_errors', '1');
         
-        return new ViewModel();
+        /**
+         * Get User module
+         */
+        $user = $this->getUserModule();
+        
+        /**
+         * If user isn't logged, redirect to login screen
+         */
+        if ($user->isUserLoggedIn()) {
+            $view = new ViewModel();
+            $view->setTemplate('application/index/index_logged_in.phtml');
+            
+            return $view;
+        }
+        
+        return new ViewModel(array());
         
     }
 
